32615683f0a864f1a251690819e0b120addee93b
jcilacad <johnchristopherilacad27@gmail.com>
2023-10-20 16:30:43 +0800

    Add fields for employee error response

--- src/main/java/com/ilacad/employeeapi/rest/EmployeeErrorResponse.java
+++ src/main/java/com/ilacad/employeeapi/rest/EmployeeErrorResponse.java
@@ -1,2 +1,18 @@
-package com.ilacad.employeeapi.rest;public class EmployeeErrorResponse {
+package com.ilacad.employeeapi.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeErrorResponse {
+
+    private Integer status;
+    private String message;
+    private Long timeStamp;
+
 }
