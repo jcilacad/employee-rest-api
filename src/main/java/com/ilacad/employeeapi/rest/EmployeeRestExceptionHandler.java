c84474090d51cfdf37e23297e36e3e9f4079f895
jcilacad <johnchristopherilacad27@gmail.com>
2023-10-20 16:40:21 +0800

    Implement the exception handler for not exisiting employee

--- src/main/java/com/ilacad/employeeapi/rest/EmployeeRestExceptionHandler.java
+++ src/main/java/com/ilacad/employeeapi/rest/EmployeeRestExceptionHandler.java
@@ -1,2 +1,23 @@
-package com.ilacad.employeeapi.rest;public class EmployeeRestExceptionHandler {
+package com.ilacad.employeeapi.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeRestExceptionHandler {
+
+    // Exception handler for employee that is not existing in database
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException (EmployeeNotFoundException exc) {
+
+        EmployeeErrorResponse errorResponse = new EmployeeErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 }
