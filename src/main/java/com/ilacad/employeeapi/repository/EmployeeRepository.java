dcbd06155ab36b215d5ddedc73967a099844b0aa
jcilacad <johnchristopherilacad27@gmail.com>
2023-10-20 14:05:05 +0800

    Change class to interface of employee

--- src/main/java/com/ilacad/employeeapi/repository/EmployeeRepository.java
+++ src/main/java/com/ilacad/employeeapi/repository/EmployeeRepository.java
@@ -1,2 +1,10 @@
-package com.ilacad.employeeapi.repository;public class EmployeeRepository {
+package com.ilacad.employeeapi.repository;
+
+import com.ilacad.employeeapi.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
