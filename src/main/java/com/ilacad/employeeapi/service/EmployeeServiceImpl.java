0109c195922e259760336dd91de30948dff4fe97
jcilacad <johnchristopherilacad27@gmail.com>
2023-10-20 14:57:53 +0800

    Add the implementation of findAll from the EmployeeRepository to EmployeeServiceImpl

--- src/main/java/com/ilacad/employeeapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/ilacad/employeeapi/service/EmployeeServiceImpl.java
@@ -1,2 +1,24 @@
-package com.ilacad.employeeapi.service;public class EmployeeServiceImpl {
+package com.ilacad.employeeapi.service;
+
+import com.ilacad.employeeapi.entity.Employee;
+import com.ilacad.employeeapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
 }
